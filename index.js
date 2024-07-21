1c6634bb955f3f70637b65011ac5c6acdaf21b98
Malcom <malcolmotieno16@gmail.com>
2024-07-21 21:41:39 +0300

    addded some comments to the code, minor changes to make code clean

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ let mysBtn = document
 window.addEventListener('scroll', function () { 
 	if (document.body.scrollTop > 20 
 		|| document.documentElement.scrollTop > 20) { 
-		mysBtn.style.display = 'block'; 
-	} else { 
+		mysBtn.style.display = 'block'; } 
+	else { 
 		mysBtn.style.display = 'none'; 
 	} 
 }); 
+
+//this javascript code is for the scroll back to top function in the rocket icon.(id='scrollbuttonid')
+//it's linked to the html document at the head section (index.js)
